85df8525a54cb9af23862684ad9d189fc68ff0cf
Changxin Sun <2979295S@student.gla.ac.uk>
2025-03-14 20:20:27 +0000

    将自定义模型注册到admin

--- VIVA_Back/myapp/admin.py
+++ VIVA_Back/myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import User
+from .models import Favorite
+from .models import Show
+
+admin.site.register(User)
+admin.site.register(Favorite)
+admin.site.register(Show)
